9016fffd3638fc59ba493806abe5249d3168f18f
nsznsnb <nsznsnb@gmail.com>
2025-01-01 17:57:27 +0900

    UIフレームワークRadzen追加

--- MyBlazorApp/Program.cs
+++ MyBlazorApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 namespace MyBlazorApp.Client
 {
@@ -12,7 +13,7 @@ namespace MyBlazorApp.Client
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddRadzenComponents();
             await builder.Build().RunAsync();
         }
     }
